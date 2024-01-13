e21e29610dc57c16c5b37b1d3bc5e5371569afce
fujisawa <fujisawa5523@gmail.com>
2024-01-13 15:46:51 +0900

    Add message acquisition process

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,43 @@
 <template>
-  <Tutorial/>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      Test Button
+    </button>
+    <div
+      v-for="(msg, index) in msgs"
+      :key="index"
+    >
+      {{ msg }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  data(): { msgs: string[] } {
+    return {
+      msgs: []
+    };
+  },
+  methods: {
+    async getMsg(): Promise<void> {
+      try {
+        const response = await this.$axios.$get('/api/general');
+        if (Array.isArray(response)) {
+          this.msgs = [...this.msgs, ...response];
+        } else {
+          console.error('Response is not an array:', response);
+        }
+      } catch (error) {
+        console.error('API call failed:', error);
+      }
+    }
+  }
+});
 </script>
